d96751f0d0bb98b2752a8600aee89056c6689c21
Riccardo Fiorista <fiorista.riccardo@gmail.com>
2022-04-05 16:52:22 +0200

    finished gtfs_aggregation.py for first download

--- eda/gtfs_aggregation.py
+++ eda/gtfs_aggregation.py
@@ -4,9 +4,8 @@ from typing import Optional, List, Tuple, Union
 import requests
 from multiprocessing.pool import ThreadPool
 import datetime
-import urbanaccess as ua
 import logging
-import zipfile
+import subprocess
 
 from osm_utils import get_bbox
 from transit_feed_providers import TransitFeedProviders
@@ -16,48 +15,16 @@ logging.basicConfig()
 logger = logging.getLogger(__file__)
 logger.setLevel(logging.INFO)
 
-
 BBOX = get_bbox(city="Amsterdam")
 DATA_PATH = './data'
+AGENCIES = ['GVB', 'IFF:GVB', 'IFF:NS', 'IFF:NSI']
 
-
-def _fetch_url_and_process_gtfs(entry: Tuple[Path, str]) -> Union[Path, GTFSDownloadException]:
-    path, uri = entry
-
-    # Download GTFS if it does not exist
-    if not os.path.exists(path):
-        r = requests.get(uri, stream=True)
-        if r.status_code == 200:
-            with open(path, 'wb') as f:
-                for chunk in r:
-                    f.write(chunk)
-        else:
-            return GTFSDownloadException(f"unable to download {uri}")
-
-    # Extract data
-    unzipped_gtfs_path = path.with_suffix('')
-    logger.info(f"Unzipping {uri} to {unzipped_gtfs_path}")
-    with zipfile.ZipFile(path, 'r') as zip_ref:
-        zip_ref.extractall(unzipped_gtfs_path)
-
-    # Load GTFS
-    loaded_feeds = ua.gtfs.load.gtfsfeed_to_df(str(unzipped_gtfs_path.absolute()),
-                                               validation=True,
-                                               verbose=True,
-                                               bbox=BBOX,
-                                               remove_stops_outsidebbox=True,
-                                               append_definitions=True)
-    # Store as HDF5
-    h5_path = path.with_suffix('.h5')
-    ua.gtfs.network.save_processed_gtfs_data(loaded_feeds, str(h5_path))
-    # Remove GTFS ZIP
-    os.remove(path)
-    return path
+ON_LISA = bool(os.environ.get("ON_LISA", False))
 
 
 def _create_gtfs_urls(provider: TransitFeedProviders,
-                     start_date: datetime.date,
-                     end_date: Optional[datetime.date] = None) -> List[Tuple[Path, str]]:
+                      start_date: datetime.date,
+                      end_date: Optional[datetime.date] = None) -> List[Tuple[Path, str]]:
     """
 
     :param provider:
@@ -85,14 +52,50 @@ def _create_gtfs_urls(provider: TransitFeedProviders,
                 )
 
 
+def _fetch_gtfs(path_and_uri: Tuple[Path, str]) -> Union[Path, GTFSDownloadException]:
+    path, uri = path_and_uri
+
+    # Download GTFS if it does not exist
+    if not os.path.exists(path):
+        r = requests.get(uri, stream=True)
+        if r.status_code == 200:
+            with open(path, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
+        else:
+            return GTFSDownloadException(f"unable to download {uri}")
+
+    return path
+
+
+def _filter_gtfs(path: Union[Path, GTFSDownloadException]) -> Union[Path, GTFSDownloadException]:
+    if isinstance(path, GTFSDownloadException):
+        return path
+    else:
+        in_path_file_ext = path.suffix
+        in_path = path.with_suffix('')
+        out_path = in_path.with_name(f"{in_path.name}-filtered-by-{'_'.join(AGENCIES)}").with_suffix(in_path_file_ext)
+
+        if not os.path.exists(out_path):
+            args = [e for sublist in [["-extract-agency", agency] for agency in AGENCIES] for e in sublist]
+            tool = "transitland" if not ON_LISA else "/home/fiorista/thesis/bin/transitland"
+            subprocess.run([tool, "extract", *args, path, out_path])
+
+        # Remove big zip
+        os.remove(path)
+
+        return out_path
+
+
 def download_and_store_gtfs(start_date: datetime.date, end_date: datetime.date, provider: TransitFeedProviders):
     urls = _create_gtfs_urls(provider, start_date, end_date)
 
     downloaded_paths = []
     total_space = 0
 
-    results = ThreadPool(8).imap_unordered(_fetch_url_and_process_gtfs, urls)
-    for path_or_exception in results:
+    results = ThreadPool(8).imap_unordered(_fetch_gtfs, urls)
+    filter_results = ThreadPool(2).imap_unordered(_filter_gtfs, results)
+    for path_or_exception in filter_results:
         if not isinstance(path_or_exception, GTFSDownloadException):
             path = path_or_exception
             logger.info(f"downloaded and processed {path}")
@@ -102,14 +105,15 @@ def download_and_store_gtfs(start_date: datetime.date, end_date: datetime.date, 
             logger.warning(str(path_or_exception))
 
     logger.info(f"###\n"
-                f"Downloaded {len(downloaded_paths)} feeds\n"
-                f"Amounting to {total_space / (1024.0*1024.0)} MB")
+                f"Processed {len(downloaded_paths)} feeds\n"
+                f"Amounting to {total_space / (1024.0 * 1024.0)} MB\n"
+                f"###")
 
 
 if __name__ == "__main__":
     # Set start and end date for feeds
     start_date = datetime.date(2019, 1, 1)
-    end_date = datetime.date(2019, 1, 8)  # datetime.date.today()
+    end_date = datetime.date(2021, 12, 31)
 
     # Start job
     download_and_store_gtfs(start_date, end_date, provider=TransitFeedProviders.GVB)
