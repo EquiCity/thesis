ddf8cacf234d82bd9221535a4f30f84404f33828
Riccardo Fiorista <fiorista.riccardo@gmail.com>
2022-04-18 15:11:30 +0200

    moved get_bbox to osm_utils.py and changed return type to be a dict

--- eda/gtfs_prep/utils/osm_utils.py
+++
@@ -1,7 +0,0 @@
-from typing import Tuple
-import osmnx as ox
-
-
-def get_bbox(city: str) -> Tuple[float, float, float, float]:
-    gdf = ox.geocode_to_gdf({'city': city})
-    return gdf.loc[0, 'bbox_west'], gdf.loc[0, 'bbox_south'], gdf.loc[0, 'bbox_east'], gdf.loc[0, 'bbox_north']
