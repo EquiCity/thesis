c3539afe78495fe222f5aa8787b5fd897afeb5d5
Riccardo Fiorista <fiorista.riccardo@gmail.com>
2022-06-14 17:06:08 +0200

    adapted optimal_baseline.py to new reward class

--- experiments/baseline_runs/optimal_baseline.py
+++ experiments/baseline_runs/optimal_baseline.py
@@ -1,18 +1,15 @@
-import matplotlib.patches
 import matplotlib.pyplot as plt
 
-from experiments.baselines.optimal import optimal_baseline
+from ptnrue.baselines.optimal_baseline import optimal_baseline
 import igraph as ig
 import numpy as np
 import geopandas as gpd
-from experiments.rewards import (
-    egalitarian_theil,
-    utilitarian,
-    elitarian,
+from ptnrue.rewards import (
+    EgalitarianTheilReward,
 )
 import logging
-from experiments.plotting.solution_plotting import plot_rewards_and_graphs
-from experiments.constants.travel_metric import TravelMetric
+from ptnrue.plotting.solution_plotting import plot_rewards_and_graphs
+from ptnrue.constants.travel_metric import TravelMetric
 
 logging.basicConfig()
 logger = logging.getLogger(__file__)
@@ -25,11 +22,11 @@ if __name__ == "__main__":
     edge_types.remove('walk')
 
     budget = 10
-    reward_func = egalitarian_theil
+    reward = EgalitarianTheilReward(census_data=census_data, com_threshold=15)
 
-    optimal_solutions = optimal_baseline(g=g, census_data=census_data, edge_types=edge_types, budget=budget,
-                                         reward_func=reward_func, metrics=[TravelMetric.TT],
-                                         com_threshold=15)
+    optimal_solutions = optimal_baseline(g=g, edge_types=edge_types,
+                                         budget=budget, reward=reward)
+
     rewards, edges = optimal_solutions[0]
 
     plot_title = f'Optimal solution with {reward.__class__.__name__} and budget size {budget}'
