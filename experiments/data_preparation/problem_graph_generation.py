393befca257b0108d72f7f8f5459a19967ce74ca
Riccardo Fiorista <fiorista.riccardo@gmail.com>
2022-07-04 17:50:22 +0200

    added data_prep jobs for 2019,2020,and 2021 and adapted problem_graph_generation.py to accept paths as a env variable

--- experiments/data_preparation/problem_graph_generation.py
+++ experiments/data_preparation/problem_graph_generation.py
@@ -1,23 +1,28 @@
+import os
 from pathlib import Path
 import geopandas as gpd
 from ptnrue.graph_generation.problem_graph_generator import ProblemGraphGenerator
 
+BASE_PATH = Path(os.environ['BASE_DATA_PATH'])
+
 
 if __name__ == "__main__":
-    AMS_DATA = Path('../ams_data/')
-
-    poi_gdf = gpd.read_file(AMS_DATA.joinpath('non_residential_functions_geojson_latlng.json'))
+    poi_gdf = gpd.read_file(BASE_PATH.joinpath('poi_latlng.json'))
     poi_gdf = poi_gdf[poi_gdf.Functie == 'Onderwijs']
     poi_gdf.geometry = gpd.points_from_xy(poi_gdf.geometry.y, poi_gdf.geometry.x, crs='EPSG:4326')
     poi_gdf['name'] = poi_gdf['Verblijfsobject']
 
-    census_gdf = gpd.read_parquet(AMS_DATA.joinpath('kwb_21_ams_neighborhoods.parquet'))
+    census_gdf = gpd.read_parquet(BASE_PATH.joinpath('neighborhoods.parquet'))
     census_gdf = gpd.GeoDataFrame(census_gdf[['BU_NAAM', 'res_centroid']], geometry='res_centroid')
     census_gdf['name'] = census_gdf['BU_NAAM']
 
     city = "Amsterdam"
-    gtfs_zip_file_path = AMS_DATA.joinpath('gtfs.zip')
-    out_dir_path = AMS_DATA.joinpath('resulting_graph/')
+    gtfs_zip_file_path = BASE_PATH.joinpath('gtfs.zip')
+    out_dir_path = BASE_PATH.joinpath('resulting_graph/')
+
+    if not os.path.exists(out_dir_path):
+        os.mkdir(out_dir_path)
+
     day = "monday"
     time_from = "07:00:00"
     time_to = "09:00:00"
